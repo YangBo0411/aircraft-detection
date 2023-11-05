d5d49171eaad1cf0060a62ede2ed3ac9190a47ea
Yang Bo <1247518345@qq.com>
2023-11-05 09:50:48 +0800

    modified

--- YOLOv7-DVC/test.py
+++ YOLOv7-DVC/test.py
@@ -289,7 +289,7 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='runs/best.pt', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default='data/aircraft dataset all.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
